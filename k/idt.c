a26cb3b0ad2e6c624662b92f50a9e7af068fde9c
Luc <luc.nguyen@epita.fr>
2023-10-25 12:12:04 +0200

    init idt asm

--- k/idt.c
+++ k/idt.c
@@ -5,18 +5,16 @@ idt_ptr_t idt_ptr;
 
 extern void idt_load(ptr);
 
-
 void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags)
 {
-    
 }
 
-void init_idt(){
+void init_idt()
+{
+    idt_ptr.limit = (sizeof(idt_entry_t) * 256) - 1;
+    idt_ptr.base = &idt;
 
+    memset(&idt, 0, sizeof(idt_entry_t) * 256);
+
+    idt_load(&idt_ptr);
 }
-
-
-
-
-
-
