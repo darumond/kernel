803bd7c01837a1f300fee7a991d5edf7decdb320
Luc <luc.nguyen@epita.fr>
2023-12-11 22:16:32 +0100

    GDT refacto

--- k/gdt.c
+++ k/gdt.c
@@ -20,12 +20,13 @@ void init_gdt()
 
 void setGate(uint32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran)
 {
-    gdt_entries[num].base_low = (base & 0xFFFF);
-    gdt_entries[num].base_middle = (base >> 16) & 0xFF;
-    gdt_entries[num].base_high = (base >> 24) & 0xFF;
+    //Base cut in 3 parts
+    gdt_entries[num].base_low = (base & 0xFFFF); //16 bits
+    gdt_entries[num].base_middle = (base >> 16) & 0xFF; //8 bits
+    gdt_entries[num].base_high = (base >> 24) & 0xFF; //8 bits
 
-    gdt_entries[num].limit= (limit & 0xFFFF);
-    gdt_entries[num].flags = (limit >> 16) & 0x0F;
+    gdt_entries[num].limit= (limit & 0xFFFF); //lower 16bits of limit
+    gdt_entries[num].flags = (limit >> 16) & 0x0F; //upper 4 bits
 
     gdt_entries[num].flags |= gran & 0xF0;
     gdt_entries[num].access = access;
